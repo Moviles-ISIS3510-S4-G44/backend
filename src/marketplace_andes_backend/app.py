1ff8ba587b33d8d6c721f10952bd82bf45d0c56b
Isaac David <idbl64@outlook.com>
2026-03-19 14:33:10 -0500

    Add bruno test

--- src/marketplace_andes_backend/app.py
+++ src/marketplace_andes_backend/app.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from contextlib import asynccontextmanager
 
 from .health.routes import router as health_router
-from .users.profile.routes import router as users_router
+from .users.routes import router as users_router
 
 
 @asynccontextmanager
