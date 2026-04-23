a8099b79058325d1189555a4b3625e8523c177d5
mariamartinez44 <mc.martinezm1@uniandes.edu.com>
2026-04-23 15:59:11 -0500

    modificacion de status history de listings

--- analytics/dbt/models/staging/stg_listing_status_history.sql
+++ analytics/dbt/models/staging/stg_listing_status_history.sql
@@ -1,7 +1,17 @@
-SELECT
-    id          AS event_id,
-    listing_id,
-    from_status,
-    to_status,
-    changed_at
-FROM {{ source('dlt_raw', 'listing_status_history') }}
+-- staging/stg_listing_status_history.sql
+with source as (
+    select * from {{ source('dlt_raw', 'listing_status_history') }}
+),
+
+renamed as (
+    select
+        id          as event_id,
+        listing_id,
+        from_status,
+        to_status,
+        changed_at,
+        date_trunc('month', changed_at)::date as changed_month
+    from source
+    )
+
+select * from renamed
